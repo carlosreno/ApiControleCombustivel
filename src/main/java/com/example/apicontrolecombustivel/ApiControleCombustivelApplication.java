8ef1717c62140c1c16bb1181e62e3360da0dca02
dev-davis <carlosdavi090787@gmail.com>
2023-07-24 14:03:21 -0300

    Add Entities Relationship Diagram

---
+++ src/main/java/com/example/apicontrolecombustivel/ApiControleCombustivelApplication.java
@@ -0,0 +1,13 @@
+package com.example.apicontrolecombustivel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleCombustivelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiControleCombustivelApplication.class, args);
+    }
+
+}
