b6b7c0fc0cbde3275b4103817408db6d01fff5b1
dev-davis <carlosdavi090787@gmail.com>
2023-07-28 17:04:36 -0300

    finalizando a migration

--- src/main/resources/db/migration/V1__CRIACAO_DO_DB_DA_APLICACAO.sql
+++ src/main/resources/db/migration/V1__CRIACAO_DO_DB_DA_APLICACAO.sql
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS cars (
     fabrication_date DATE,
     comments TEXT,
     company_id INT,
-    availability_id INT,
+    active_availability_id INT,
     status ENUM('active','disabled')
 );
 
@@ -31,9 +31,9 @@ CREATE TABLE IF NOT EXISTS sector (
 );
 CREATE TABLE IF NOT EXISTS availability (
                                       id INT AUTO_INCREMENT PRIMARY KEY,
-                                      name VARCHAR(100) NOT NULL,
                                       car_id INT,
-                                      sector_id INT
+                                      sector_id INT,
+                                      status ENUM('active','disable')
 );
 CREATE TABLE IF NOT EXISTS user (
                                       id INT AUTO_INCREMENT PRIMARY KEY,
@@ -55,8 +55,8 @@ CREATE TABLE IF NOT EXISTS contract (
                           value DECIMAL(10, 2),
                           customer VARCHAR(100),
                           status ENUM('Active', 'Closed', 'Canceled', 'Suspended'),
-                          supplier_id INT,
-                          comments TEXT
+                          comments TEXT,
+                          supplier_id INT
 );
 
 CREATE TABLE IF NOT EXISTS supplier (
@@ -77,20 +77,61 @@ CREATE TABLE IF NOT EXISTS user_sector (
                                FOREIGN KEY (sector_id) REFERENCES sector(id)
 );
 
-ALTER TABLE cars ADD CONSTRAINT company_id
+CREATE TABLE IF NOT EXISTS requests (
+                                        id INT AUTO_INCREMENT PRIMARY KEY,
+                                        request_date DATE NOT NULL,
+                                        fuel_amount DECIMAL(10, 2) NOT NULL,
+                                        comments TEXT,
+                                        status ENUM('Pending', 'Approved', 'Rejected', 'Completed') NOT NULL,
+                                        car_id INT NOT NULL,
+                                        requester_id INT NOT NULL,
+                                        contract_id INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS fueling (
+                                       id INT AUTO_INCREMENT PRIMARY KEY,
+                                       fuel_date DATE NOT NULL,
+                                       fuel_amount DECIMAL(10, 2) NOT NULL,
+                                       price_per_unit DECIMAL(10, 2) NOT NULL,
+                                       total_cost DECIMAL(10, 2) NOT NULL,
+                                       comments TEXT,
+                                       car_id INT NOT NULL,
+                                       request_id INT,
+                                       contract_id INT NOT NULL
+);
+
+ALTER TABLE cars
+    ADD CONSTRAINT fk_activate_availability_id
+    FOREIGN KEY (active_availability_id) references availability(id),
+
+    ADD CONSTRAINT fk1_company_id
     FOREIGN KEY (company_id) references company(id);
 
-ALTER TABLE contract ADD CONSTRAINT supplier_id
+
+ALTER TABLE contract
+    ADD CONSTRAINT fk_supplier_id
     FOREIGN KEY (supplier_id) references supplier(id);
 
-ALTER TABLE availability ADD CONSTRAINT sector_id
-    FOREIGN KEY (sector_id) references sector(id);
+ALTER TABLE availability
+    ADD CONSTRAINT fk_sector_id FOREIGN KEY (sector_id) references sector(id),
+    ADD CONSTRAINT car_id FOREIGN KEY (car_id) references cars(id);
 
-ALTER TABLE availability ADD CONSTRAINT car_id
-    FOREIGN KEY (car_id) references cars(id);
-
-ALTER TABLE user ADD CONSTRAINT user_type_id
-    FOREIGN KEY (user_type) references user_type(id);
-
-ALTER TABLE sector ADD CONSTRAINT company_id
+ALTER TABLE user
+    ADD CONSTRAINT fk_user_type_id
+    FOREIGN KEY (user_type) references user_type(id),
+    ADD CONSTRAINT fk_company_id
     FOREIGN KEY (company_id) references company(id);
+
+ALTER TABLE sector
+    ADD CONSTRAINT fk2_company_id
+    FOREIGN KEY (company_id) references company(id);
+
+ALTER TABLE fueling
+      ADD CONSTRAINT fk_fueling_car_id FOREIGN KEY (car_id) REFERENCES cars(id),
+      ADD CONSTRAINT fk1_contract_id FOREIGN KEY (contract_id) REFERENCES contract(id),
+      ADD CONSTRAINT fk_request_id FOREIGN KEY (request_id) REFERENCES requests(id);
+
+ALTER TABLE requests
+        ADD CONSTRAINT fk_car_id FOREIGN KEY (car_id) REFERENCES cars(id),
+        ADD CONSTRAINT fk_requester_id FOREIGN KEY (requester_id) REFERENCES user(id),
+        ADD CONSTRAINT fk1_contract_id FOREIGN KEY (contract_id) REFERENCES contract(id);
